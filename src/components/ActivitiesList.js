0177b99c7cb29cafdfddd93148895e52b3f40387
OussamaAlouat <al317257@uji.es>
2019-01-10 23:28:54 +0100

    Used display  block intead of flex and classes to display lists

--- src/components/ActivitiesList.js
+++ src/components/ActivitiesList.js
@@ -5,6 +5,6 @@ const ActivitiesList = (props) =>  {
     const activitiesToDo = props.activitiesToDo.map((activity) =>{
        return <ActivityItem key={activity.id} activity={activity}></ActivityItem>
     });
-    return <div>{activitiesToDo}</div>
+    return <div className="ui list">{activitiesToDo}</div>
 };
 export default ActivitiesList;
--- src/components/ActivityItem.js
+++ src/components/ActivityItem.js
@@ -3,9 +3,9 @@ import React from 'react'
 class ActivityItem extends React.Component {
     render() {
         return (
-            <div style={{width:'100%', alignItems:'center'}} >
-                <i className="large tasks middle aligned icon"></i>
+            <div className="item" >
                 <div className="content">
+                    <i className="large tasks middle aligned icon"></i>
                     {this.props.activity.value}
                 </div>
             </div>
