54d372ca2ee0f3d73f022917fc4dfd6160c3d5cd
OussamaAlouat <al317257@uji.es>
2022-09-15 21:35:22 +0200

    Adapt the identation

--- src/components/ActivitiesList.jsx
+++ src/components/ActivitiesList.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import ActivityItem from "./ActivityItem";
 
 const ActivitiesList = (props) => {
+  const onRemove = (item) => {
+    props.onRemove(item)
+  };
 
-    const onRemove = (item) => {
-        props.onRemove(item)
-    };
+  const onComplete = item => {
+    props.onComplete(item)
+  };
 
-    const onComplete = item => {
-        props.onComplete(item)
-    };
+  const activitiesToDo = props.activitiesToDo.map((activity) => {
+    return <ActivityItem onComplete={onComplete} onRemove={onRemove} key={activity.id} activity={activity}></ActivityItem>
+  });
 
-    const activitiesToDo = props.activitiesToDo.map((activity) => {
-        return <ActivityItem onComplete={onComplete} onRemove={onRemove} key={activity.id} activity={activity}></ActivityItem>
-    });
-    return <div className="ui list">{activitiesToDo}</div>
+  return <div className="ui list">{activitiesToDo}</div>
 };
+
 export default ActivitiesList;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,49 +5,52 @@ import './App.css'
 
 class App extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            activities: []
-        };
-    }
-
-
-    onRemove = (item) => {
-        const provisionalActivities = this.state.activities;
-        this.setState({activities: provisionalActivities.filter((val) => val.id !== item.id)})
+  constructor() {
+    super();
+    this.state = {
+        activities: []
     };
+  }
 
-    setActivity = term => {
-        this.setState(state => {
-            const activities = [...state.activities, term];
-            return {
-                activities
-            }
-        });
-    };
 
-    onComplete = term => {
-        const provisionalActivities = this.state.activities;
-        const data = provisionalActivities.map((val) => {
-            val.completed = val.id === term.id ? true : val.completed;
-            return val
-        });
+  onRemove = (item) => {
+    const provisionalActivities = this.state.activities;
+    this.setState({activities: provisionalActivities.filter((val) => val.id !== item.id)})
+  };
 
-        this.setState({
-            activities: data
-        });
-    };
+  setActivity = term => {
+    this.setState(state => {
+      const activities = [...state.activities, term];
+      return {
+        activities
+      }
+    });
+  };
 
-    render() {
-        return (
-            <div className=" ui container app">
-                <ActivitiesList onComplete={this.onComplete} onRemove={this.onRemove}
-                                activitiesToDo={this.state.activities}/>
-                <SearchBar onClick={this.setActivity}/>
-            </div>
-        )
-    }
+  onComplete = term => {
+    const provisionalActivities = this.state.activities;
+    const data = provisionalActivities.map((val) => {
+      val.completed = val.id === term.id ? true : val.completed;
+      return val
+    });
+
+    this.setState({
+      activities: data
+    });
+  };
+
+  render() {
+    return (
+      <div className=" ui container app">
+        <ActivitiesList 
+          onComplete={this.onComplete} onRemove={this.onRemove}
+          activitiesToDo={this.state.activities}
+        />
+
+        <SearchBar onClick={this.setActivity}/>
+      </div>
+    )
+  }
 
 };
 export default App;
